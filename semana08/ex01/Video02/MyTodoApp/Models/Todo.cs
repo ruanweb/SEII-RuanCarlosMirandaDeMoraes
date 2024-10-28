5ae7be468d8018b3cfa16ad1b088b73790ff1b2d
ruanweb <ruancarlosmdm@gmail.com>
2024-10-27 22:39:22 -0300

    Add files via upload

---
+++ semana08/ex01/Video02/MyTodoApp/Models/Todo.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+
+namespace MyTodoApp.Models
+{
+    public class Todo
+    {
+        public int Id { get; set; }
+
+        [DisplayName("Title")]
+        [Required(ErrorMessage = "Required Field")]
+        public string Title { get; set; }
+
+        [DisplayName("Done")]
+        public bool Done { get; set; }
+
+        [DisplayName("Created in")]
+        public DateTime CreatedAt { get; set; } = DateTime.Now;
+
+        [DisplayName("Last Update in")]
+        public DateTime LastUpdateDate { get; set; } = DateTime.Now;
+        public string User {  get; set; }
+    }
+}
